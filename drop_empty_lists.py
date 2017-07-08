7226061f3c1b13131dcce9184f99659809bad752
vendi <svitlana.vakulenko@gmail.com>
2017-07-08 08:41:52 +0200

    new: procedure to cleanup the lists list

---
+++ drop_empty_lists.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Jul 8, 2017
+
+.. codeauthor: svitlana vakulenko
+    <svitlana.vakulenko@gmail.com>
+
+'''
+
+from create_community_list import drop_my_empty_lists
+
+if __name__ == '__main__':
+    drop_my_empty_lists()
