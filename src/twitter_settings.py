251a3bedc983a84295564165b0aedecb5945039b
Svitlana Vakulenko <svitlana.vakulenko@gmail.com>
2017-08-04 19:08:30 +0200

    Delete twitter_settings.py

--- src/twitter_settings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author: svakulenko 8/07/17
-
-APP_KEY = 'aX8O7b59mlVqvBd1naXFoW5QI'
-APP_SECRET = 'xtCPkGLUoHougMkFYGVz5aRvrbvJ8Ha8VlJnI9Ns7qBFCjiX4R'
-OAUTH_TOKEN = '955599008-qB59O77Y2oVvCs419OkvgeySC1cyMRumPOtysbX5'
-OAUTH_TOKEN_SECRET = '0zb4JBa40gEgZU7OxKXCglCOIycrLe6lAhkdWN1pk9pKP'
