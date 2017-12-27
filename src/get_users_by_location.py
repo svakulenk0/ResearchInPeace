92cf43e8e2c69dbe7703704880b944ea322c091b
vendi <svitlana.vakulenko@gmail.com>
2017-12-27 11:38:15 +0100

    new: clean up shared list

--- src/get_users_by_location.py
+++ src/get_users_by_location.py
@@ -28,7 +28,7 @@ LISTS = ['kyiv', 'wien', 'moscow', 'Nyc', 'amsterdam']
 
 LOCAL_COMMUNITIES = {
     'kyiv': ['kyiv', 'київ', 'киев'],
-    'wien_2': ['wien', 'vienna'],
+    'wien2': ['wien', 'vienna'],
     'moscow': ['moscow', 'москва'],
     'Nyc': ['New York City'],
     'amsterdam': ['amsterdam'],
@@ -40,22 +40,34 @@ def get_users_by_location(locations, keyword, list):
     TP.collect_members_based_on_location(locations, keyword, list_name=list)
 
 
-def expand_list_with_friends(location):
+def clean_up_distributed_list(list1='wien', list2='wien2'):
+    TP = Twitter_Processor()
+    members1 = set([member['screen_name'] for member in TP.get_list_members(list1)])
+    members2 = set([member['screen_name'] for member in TP.get_list_members(list2)])
+    duplicates = list(members1 & members2)
+    if duplicates:
+        TP.delete_list_members(list2, duplicates)
+
+
+def expand_list_with_friends(location, oldlists=['wien']):
     list_name = location
     keywords = LOCAL_COMMUNITIES[list_name]
     TP = Twitter_Processor()
     members = TP.get_list_members(list_name)
+    for oldlist in oldlists:
+        members += TP.get_list_members(oldlist)
+    members_names = [member['screen_name'] for member in members]
     # shuffle the list of users randomly
-    for member in np.random.permutation(members):
-        print "User:", member['screen_name']
+    for member in np.random.permutation(members_names):
+        print "User:", member
         # get friends
-        friends = TP.retrieve_friends_of(member['screen_name'], 200)
+        friends = TP.retrieve_friends_of(member, 200)
         print "#Friends:", len(friends)
         # check user-specified location
         users = [user['screen_name'] for keyword in keywords
-               for user in friends for location in user['location'].split() if keyword in location.lower().encode('utf-8')]
-        print "#Friends in " + list_name +":", len(users)
-        TP.add_to_list(set(users), list_name)
+                 for user in friends for location in user['location'].split() if keyword in location.lower().encode('utf-8')]
+        new_users = set(users) - set(members_names)
+        TP.add_to_list(new_users, list_name, deduplicate=False)
         # to avoid Twitter block
         # time.sleep(2000)
 
@@ -72,10 +84,9 @@ def test_get_users_by_location():
 
 
 if __name__ == '__main__':
-    # test_get_users_by_location()
     while True:
         try:
-            expand_list_with_friends(location='wien_2')
+            expand_list_with_friends(location='wien2')
         except TwythonRateLimitError as e:
             print "TwythonRateLimitError... Waiting for 2000 seconds"
             time.sleep(2000)
